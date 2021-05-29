7e15ccbb7977e7fab9922b8b7bf9a1aa1418da6e
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-29 08:54:02 -0700

    added js to insert into HTML

--- public/js/questionTemplate.js
+++ public/js/questionTemplate.js
@@ -9,9 +9,9 @@ templates['question'] = template({"compiler":[8,">= 4.3.0"],"main":function(cont
     };
 
   return "<article class=\"post\">\n    <div class=\"question-content\">\n        <p class=\"content\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"content") || (depth0 != null ? lookupProperty(depth0,"content") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"content","hash":{},"data":data,"loc":{"start":{"line":4,"column":12},"end":{"line":4,"column":23}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"questionText") || (depth0 != null ? lookupProperty(depth0,"questionText") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"questionText","hash":{},"data":data,"loc":{"start":{"line":4,"column":12},"end":{"line":4,"column":28}}}) : helper)))
     + "\n        </p>\n        <p class=\"author\">\n            "
-    + alias4(((helper = (helper = lookupProperty(helpers,"author") || (depth0 != null ? lookupProperty(depth0,"author") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data,"loc":{"start":{"line":7,"column":12},"end":{"line":7,"column":22}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"questionName") || (depth0 != null ? lookupProperty(depth0,"questionName") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"questionName","hash":{},"data":data,"loc":{"start":{"line":7,"column":12},"end":{"line":7,"column":28}}}) : helper)))
     + "\n        </p>\n    </div>\n</article>";
 },"useData":true});
 })();
