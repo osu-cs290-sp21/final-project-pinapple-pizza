0e6f0b046d84ffd5f6c2b8c7bc8afad45630b9dc
aaronmichaelfrost <aaronmichaelfrost@gmail.com>
2021-06-09 12:37:38 -0700

    Button hover CSS

--- public/js/queueEntryTemplate.js
+++ public/js/queueEntryTemplate.js
@@ -8,10 +8,10 @@ templates['queueEntry'] = template({"1":function(container,depth0,helpers,partia
         return undefined
     };
 
-  return "<td class=\"close-button-cell\"><button type=\"button\" class=\"queue-remove-button\" onclick=\"queueRemoveClicked("
-    + alias4(((helper = (helper = lookupProperty(helpers,"position") || (depth0 != null ? lookupProperty(depth0,"position") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"position","hash":{},"data":data,"loc":{"start":{"line":6,"column":128},"end":{"line":6,"column":140}}}) : helper)))
+  return "<td class=\"close-button-cell\"><button type=\"button\" class=\"queue-remove-button selectable\" onclick=\"queueRemoveClicked("
+    + alias4(((helper = (helper = lookupProperty(helpers,"position") || (depth0 != null ? lookupProperty(depth0,"position") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"position","hash":{},"data":data,"loc":{"start":{"line":6,"column":139},"end":{"line":6,"column":151}}}) : helper)))
     + ", '"
-    + alias4(((helper = (helper = lookupProperty(helpers,"password") || (depth0 != null ? lookupProperty(depth0,"password") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"password","hash":{},"data":data,"loc":{"start":{"line":6,"column":143},"end":{"line":6,"column":155}}}) : helper)))
+    + alias4(((helper = (helper = lookupProperty(helpers,"password") || (depth0 != null ? lookupProperty(depth0,"password") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"password","hash":{},"data":data,"loc":{"start":{"line":6,"column":154},"end":{"line":6,"column":166}}}) : helper)))
     + "')\"><i class=\"fas fa-times\"></i></button></td>";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -30,7 +30,7 @@ templates['queueEntry'] = template({"1":function(container,depth0,helpers,partia
     + "</td>\r\n    <td>"
     + alias4(((helper = (helper = lookupProperty(helpers,"reqType") || (depth0 != null ? lookupProperty(depth0,"reqType") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"reqType","hash":{},"data":data,"loc":{"start":{"line":5,"column":8},"end":{"line":5,"column":19}}}) : helper)))
     + "</td>\r\n    "
-    + ((stack1 = lookupProperty(helpers,"if").call(alias1,(depth0 != null ? lookupProperty(depth0,"password") : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":4},"end":{"line":6,"column":208}}})) != null ? stack1 : "")
+    + ((stack1 = lookupProperty(helpers,"if").call(alias1,(depth0 != null ? lookupProperty(depth0,"password") : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":4},"end":{"line":6,"column":219}}})) != null ? stack1 : "")
     + "\r\n</tr>";
 },"useData":true});
 })();
